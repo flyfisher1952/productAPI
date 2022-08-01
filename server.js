39b02d0ed27f0a1145ef5f4b1a61bf9f2dd4605c
flyfisher1952 <mike.lockhart@mail.com>
2022-08-01 06:03:37 -0600

    fixed issue with body-parser

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require("express");
 var app = express();
-
-var productAPI = require("./controllers/product.controller");
-app.use("/api/products", productAPI);
-
 var bodyparser = require("body-parser");
+
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 
+var productAPI = require("./controllers/product.controller");
+
+app.use("/api/products", productAPI);
 app.listen(8080);
 
 console.log("Product API listening on 8080");
