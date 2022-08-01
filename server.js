9cc5fc5d268ca27c660878659d993cdae267024d
flyfisher1952 <mike.lockhart@mail.com>
2022-08-01 06:08:48 -0600

    added a comment/reminder

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var bodyparser = require("body-parser");
 
+// body-parser MUST be added BEFORE the controller 'app.use' statement or it won't work.
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: false }));
 
